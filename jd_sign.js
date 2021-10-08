6c41b61a67338886ab43fc05bafd75cf22b449dd
赵安家 <anjia0532@gmail.com>
2021-10-08 12:23:20 +0800

    支持钉钉机器人通知

--- jd_sign.js
+++ jd_sign.js
@@ -11,9 +11,8 @@ const download = require('download')
 const cookie = process.env.JD_COOKIE
 // 钉钉群机器人token
 const push_key = process.env.PUSH_KEY
-
 // 京东脚本文件
-const js_url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js'
+const js_url = 'https://raw.fastgit.org/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js'
 // 下载脚本路劲
 const js_path = './JD_DailyBonus.js'
 // 脚本执行输出路劲
@@ -22,99 +21,94 @@ const result_path = './result.txt'
 const error_path = './error.txt'
 
 Date.prototype.Format = function (fmt) {
-  var o = {
-    'M+': this.getMonth() + 1,
-    'd+': this.getDate(),
-    'H+': this.getHours(),
-    'm+': this.getMinutes(),
-    's+': this.getSeconds(),
-    'S+': this.getMilliseconds()
-  };
-  if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
-  }
-  for (var k in o) {
-    if (new RegExp('(' + k + ')').test(fmt)) {
-      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (('00' + o[k]).substr(String(o[k]).length)));
+    var o = {
+        'M+': this.getMonth() + 1,
+        'd+': this.getDate(),
+        'H+': this.getHours(),
+        'm+': this.getMinutes(),
+        's+': this.getSeconds(),
+        'S+': this.getMilliseconds()
+    };
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
     }
-  }
-  return fmt;
+    for (var k in o) {
+        if (new RegExp('(' + k + ')').test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (('00' + o[k]).substr(String(o[k]).length)));
+        }
+    }
+    return fmt;
 };
 
 function setupCookie() {
-  let cookies = cookie.split("|")
-
-  var js_content = fs.readFileSync(js_path, 'utf8')
-  js_content = js_content.replace(/var Key = ''/, `var Key = '${cookies[0]}'`)
-  if (cookies.length > 1) {
-    js_content = js_content.replace(/var DualKey = ''/, `var DualKey = '${cookies[1]}'`)
-  }
-  if (cookies.length > 2) {
-    let others = []
-    for (let i = 2; i < cookies.length; i++) {
-      others.push({ "cookie": cookies[i] })
+    let js_content = fs.readFileSync(js_path, 'utf8')
+    let cookie_str_array = cookie.split("|")
+    let cookie_json_array = []
+    for (let i = 0; i < cookie_str_array.length; i++) {
+        cookie_json_array.push({"cookie": cookie_str_array[i]})
     }
-    let other_key = JSON.stringify(others)
-    js_content = js_content.replace(/var OtherKey = ''/, `var OtherKey = '${other_key}'`)
-  }
-  fs.writeFileSync(js_path, js_content, 'utf8')
+    js_content = js_content.replace(/var OtherKey = ``/, `var OtherKey = \`${JSON.stringify(cookie_json_array)}\``)
+    fs.writeFileSync(js_path, js_content, 'utf8')
 }
 
 function sendNotificationIfNeed() {
 
-  if (!push_key) {
-    console.log('执行任务结束!'); return;
-  }
-
-  if (!fs.existsSync(result_path)) {
-    console.log('没有执行结果，任务中断!'); return;
-  }
-  let desp = fs.readFileSync(result_path, "utf8")
-
-  // 去除末尾的换行
-  let TOKEN = push_key.replace(/[\r\n]/g, "")
-
-  const options = {
-    uri: `https://oapi.dingtalk.com/robot/send?access_token=${TOKEN}`,
-    body: { "msgtype": "text", "text": { "content": desp } },
-    json: true,
-    method: 'POST'
-  }
-
-  rp.post(options).then(res => {
-    const code = res['errcode'];
-    if (code == 0) {
-      console.log("通知发送成功，任务结束！")
+    if (!push_key) {
+        console.log('执行任务结束!');
+        return;
     }
-    else {
-      console.log(res);
-      console.log("通知发送失败，任务中断！")
-      fs.writeFileSync(error_path, JSON.stringify(res), 'utf8')
+
+    if (!fs.existsSync(result_path)) {
+        console.log('没有执行结果，任务中断!');
+        return;
     }
-  }).catch((err) => {
-    console.log("通知发送失败，任务中断！")
-    fs.writeFileSync(error_path, err, 'utf8')
-  })
+    let text = "京东签到_" + dateFormat();
+    let desp = fs.readFileSync(result_path, "utf8")
+
+    // 去除末尾的换行
+    let TOKEN = push_key.replace(/[\r\n]/g, "")
+
+    const options = {
+        uri: `https://oapi.dingtalk.com/robot/send?access_token=${TOKEN}`,
+        body: {"msgtype": "markdown", "markdown": {"title": text, "text": desp}},
+        json: true,
+        method: 'POST'
+    }
+
+    rp.post(options).then(res => {
+        const code = res['errcode'];
+        if (code === 0) {
+            console.log("通知发送成功，任务结束！")
+        } else {
+            console.log(res);
+            console.log("通知发送失败，任务中断！")
+            fs.writeFileSync(error_path, JSON.stringify(res), 'utf8')
+        }
+    }).catch((err) => {
+        console.log("通知发送失败，任务中断！")
+        fs.writeFileSync(error_path, err, 'utf8')
+    })
 }
 
 async function main() {
 
-  if (!cookie) {
-    console.log('请配置京东cookie!'); return;
-  }
+    if (!cookie) {
+        console.log('请配置京东cookie!');
+        return;
+    }
 
-  // 1、下载脚本
-  download(js_url, './').then(res => {
-    // 2、替换cookie
-    setupCookie()
-    // 3、执行脚本
-    exec(`node '${js_path}' > '${result_path}'`);
-    // 4、发送推送
-    sendNotificationIfNeed()
-  }).catch((err) => {
-    console.log('脚本文件下载失败，任务中断！');
-    fs.writeFileSync(error_path, err, 'utf8')
-  })
+    // 1、下载脚本
+    download(js_url, './').then(res => {
+        // 2、替换cookie
+        setupCookie()
+        // 3、执行脚本
+        exec(`node '${js_path}' > '${result_path}'`);
+        // 4、发送推送
+        sendNotificationIfNeed()
+    }).catch((err) => {
+        console.log('脚本文件下载失败，任务中断！');
+        fs.writeFileSync(error_path, JSON.stringify(err), 'utf8')
+    })
 }
 
 main()
